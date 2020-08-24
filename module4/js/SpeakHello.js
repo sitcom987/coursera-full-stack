9b15c27032506cea57ae1ac73c431abc7a14d410
Adam Szklenar <szkl.adam@adams-mbp.home>
2020-08-24 23:06:36 +0100

    one more

--- module4/js/SpeakHello.js
+++ module4/js/SpeakHello.js
@@ -1,6 +1,7 @@
 (function (window) {
-	var speakWord = "Hello";
 	var helloSpeaker = {};
+	var speakWord = "Hello";
+	
 	helloSpeaker.speak = function (name) {
 		console.log(speakWord + " " + name);
 	}
